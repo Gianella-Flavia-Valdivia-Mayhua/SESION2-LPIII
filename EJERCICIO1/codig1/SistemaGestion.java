8f343983dd4f41d34facee8f013cac0680cf7955
Gianella-Flavia-Valdivia-Mayhua <gianellavaldiviamayhua@gmail.com>
2026-08-27 01:12:38 -0500

    Implement student enrollment and details display
    Added student registration and details display functionality.

--- EJERCICIO1/codig1/SistemaGestion.java
+++ EJERCICIO1/codig1/SistemaGestion.java
@@ -9,5 +9,18 @@ public class SistemaGestion {
 
         curso1.asignarProfesor(prof1);
         curso2.asignarProfesor(prof2);
-}
+        
+        Estudiante est1 = new Estudiante("E01", "Juan Perez", 15.5, 14.0);
+        Estudiante est2 = new Estudiante("E02", "Maria Lopez", 18.0, 19.5);
+        Estudiante est3 = new Estudiante("E03", "Pedro Gomez", 11.0, 13.5);
+
+        curso1.inscribirEstudiante(est1);
+        curso1.inscribirEstudiante(est2);
+        curso2.inscribirEstudiante(est3);
+
+        curso1.mostrarDetalles();
+        curso2.mostrarDetalles();
+
+        System.out.println("\nTotal de estudiantes registrados en el sistema: " + Estudiante.getTotalEstudiantes());
+    }
 }
