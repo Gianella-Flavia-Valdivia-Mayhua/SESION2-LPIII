0d5e82d6b9d0f765abe29babc06a18c342f82597
Gianella-Flavia-Valdivia-Mayhua <gianellavaldiviamayhua@gmail.com>
2026-08-27 01:09:36 -0500

    Create Categoria.java

---
+++ EJERCICIO1/codig1/Categoria.java
@@ -0,0 +1,12 @@
+package codig1;
+class Categoria {
+    private String nombreCategoria;
+
+    public Categoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+}
